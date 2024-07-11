d9f74cb75e5ea1bb3ed135f0b0ef8532680231f1
Aryan jaswal <75902876+aryan631@users.noreply.github.com>
2024-07-11 09:27:21 +0000

    Conversion of Data types

---
+++ 01_basics/conversionOpertion.js
@@ -0,0 +1,22 @@
+let score= true
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber=Number(score)
+//console.log(valueInNumber)
+//console.log(typeof(valueInNumber));
+
+// "33"=>33
+// "33abc"=> Nan
+// true =>1;false =>0
+let isLoggedIn=1
+let BooleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(x);
+// 1=>true; 0=>false
+// "" =>false
+// "hitesh" =>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber))
