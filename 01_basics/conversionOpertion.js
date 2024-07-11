64fa0ffe03e4fecf8dcdc9bb33c347be97f88df3
Aryan jaswal <75902876+aryan631@users.noreply.github.com>
2024-07-11 10:54:16 +0000

    Prefix and postfix

--- 01_basics/conversionOpertion.js
+++ 01_basics/conversionOpertion.js
@@ -18,5 +18,40 @@ let BooleanIsLoggedIn=Boolean(isLoggedIn)
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber))
+//console.log(stringNumber);
+//console.log(typeof(stringNumber))
+
+// **************************************OPERATIONS*****************************************
+let value=3;
+let negValue=-value;
+//console.log(negValue)
+//console.log(typeof(negValue));
+//console.log(2+2)
+//console.log(2*2);
+//console.log(2/2);
+//console.log(4%3);
+//console.log(2**4);
+
+let str1=1;
+let str2=2;
+let str3=str1+str2;
+console.log(str3);
+
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+//console.log((3+4)*5 %3)
+let gameCounter=50;
+gameCounter++;
+console.log(gameCounter);
+
+//Postfix Operator
+let x=4;
+const y=x++;
+console.log(`x:${x},y:${y}`);
+//if used postfix, with operator after operand(for example, x++)the increment operator increments
+//and returns the value before incrementing
+ let a=5;
+ const b=++a;
+ console.log(`a:${a},b:${b}`);
